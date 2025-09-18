64a15c2f729f5c14a25ece1e31bcdfc8e1a745fa
wexuan261221 <wbhydne711001@gmail.com>
2025-09-18 21:13:36 +0200

    Initial commit for Lab1B

---
+++ semaphores.adb
@@ -0,0 +1,12 @@
+-- Package: Semaphores
+--
+-- ==> Complete the code at the indicated places
+
+package body Semaphores is
+   protected body CountingSemaphore is
+      entry Wait -- To be completed
+
+      entry Signal -- To be completed
+   end CountingSemaphore;
+end Semaphores;
+
